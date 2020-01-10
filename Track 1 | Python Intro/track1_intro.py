01a656da176a7eab9e24e6d5277a42a36241cd0b
Pawan Yaddanapudi <pawanyaddanapudi@Pawans-MacBook-Pro-2.local>
2020-01-10 17:52:24 +0900

    Update

---
+++ Track 1 | Python Intro/track1_intro.py
@@ -0,0 +1,25 @@
+# Python Introduction
+
+# Which version of Python are you in
+
+
+# Get into Python shell
+
+
+# How to run code in Python
+
+# Modules and import them
+
+# Variables
+
+# Data Types : Numbers, Strings, Casting, Booleans
+
+
+# Data structures: Lists, Tuples, Sets, Dictionaries
+
+# Loops: If...Else, While, For
+
+# Functions: UDF, Lambda
+
+
+
